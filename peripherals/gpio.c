f7fcd6e94c767ccc469d15ffb6585bfa38fac108
DimDam55 <62395248+DimDam55@users.noreply.github.com>
2023-01-02 16:24:57 +0200

    Update gpio.c

--- peripherals/gpio.c
+++ peripherals/gpio.c
@@ -1,6 +1,9 @@
 #include "gpio.h"
 #include <stdint.h>
 
+/*-------------------------------------------------------------------------------------------------------------------*/
+/*    GPIOs    */
+/*-------------------------------------------------------------------------------------------------------------------*/
 
 #define CONFIG(GPIOx_CRL, GPIOx_CRH) {																	\
 	switch(gpio_mode){																										\
@@ -14,7 +17,7 @@
 			break;																														\
 		case ALT_FUNC_PUSH_PULL:																						\
 			modey = 0b11;																											\
-			cnfy = 0b00;																											\
+																													\
 			cnfy = 0b10;																											\
 			break;																														\
 		case ALT_FUNC_OPEN_DRAIN:																						\
@@ -78,4 +81,30 @@ void setPin(uint8_t port, uint8_t pin, uint8_t value){
 		default:
 			break;
 	}
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+/*    TIMERS    */
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+void setTimer(uint32_t timer,uint8_t direction,uint16_t value){
+  uint32_t *curr; //pointer to registers addresses
+
+  curr = timer+TIMX_DIER;
+  *curr = 0x0001; //enable update interrupt
+
+  curr = timer+TIMX_ARR;
+  *curr = value; //set reload value
+
+  curr = timer+TIMX_CR1;
+  *curr |= (direction<<4)|(0b1); //set direction bit and CEN=1
+}
+
+void setPWM(uint32_t timer, uint32_t duty_cycle, uint32_t freq){
+	setTimer(timer,UP,freq);
+	uint32_t *curr;
+
+	curr = timer+TIMx_CCR1; //using just the 1st channel
+	*curr = duty_cycle;
+
 }
