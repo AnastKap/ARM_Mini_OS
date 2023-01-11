741a8c6dc67045143089990350c1ed2428ea7456
DimDam55 <62395248+DimDam55@users.noreply.github.com>
2023-01-11 23:17:34 +0200

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -5,6 +5,7 @@
 #include "process/process.h"
 #include "memory_management/memory.h"
 #include "clock/clock.h"
+#include "peripherals/timer.h"
 #include <stdint.h>
 
 
@@ -57,16 +58,53 @@ void testADC(){
 	}
 }
 
+void testTIM2(){
+	//it works fine on debug: counter and status change value
+	//on flash it seems like there is no delay
+	uint32_t *cr1 = &TIM2_CR1;
+	uint32_t *status = &TIM2_SR;
+	uint32_t *counter = &TIM2_CNT;
+
+	TIM2_ARR = 0xFFFF;
+	TIM2_PSC = 0xFFFF;
+	TIM2_CR1 = (0<<CMS1)|(0<<CMS1)|(0<<DIR)|(0<<URS)|(0<<UDIS)|(1<<CEN);
+
+	/*
+	TIMx_PSC is loaded its proper value after the first update event.
+	This requires a "false" first update event.
+
+	TIMx_SR bits are cleared by software and set by hardware.
+	*/
+
+	if((TIM2_SR && (1<<UIF)==0x01)) TIM2_SR &=0x00; //clear SR after first, "false" event
+
+	while(1){
+		if((TIM2_SR && (1<<UIF)) == 0x01){
+			TIM2_SR &= 0x00; //manually clear SR
+			demo();
+		}
+	}
+}
+
+void demo(){
+	while(1){
+		uint32_t i;
+		setPin(GPIOC, GPIO_PIN_13, 1);
+		for (i = 0; i < 75000; i++);
+		setPin(GPIOC, GPIO_PIN_13, 0);
+		for (i = 0; i < 75000; i++);
+	}
+}
 
 void kernel(){
-	disableUnalignedAddressFault();
-	initDallocSpace();
-	initProcessPages();
+	//disableUnalignedAddressFault();
+	//initDallocSpace();
+	//initProcessPages();
 	enable_external_clock();
 	enable_gpio_clocks();
 	enable_timer_clocks();
 	enable_adc_clocks();
-	enable_external_clock_pll(4);
+	//enable_external_clock_pll(4);
 
 	configPin(GPIOB, GPIO_PIN_12, OUTPUT_PUSH_PULL);
 	for(int i = 0; i < 8; i++){
@@ -74,6 +112,7 @@ void kernel(){
 	}
 	configPin(GPIOC, GPIO_PIN_13, OUTPUT_PUSH_PULL);
 
+	/*
 	startScheduler();
 	createNewProcess(process1);
 	//createNewProcess(process2);
@@ -81,4 +120,6 @@ void kernel(){
 	createNewProcess(testADC);
 
 	process1();
+	*/
+	testTIM2();
 }
