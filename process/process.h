b06bc4c0ab03a9b848890b76702cc27e4b93e218
DimDam55 <62395248+DimDam55@users.noreply.github.com>
2023-02-14 15:44:10 +0200

    Implementation of IPC/pipes

--- process/process.h
+++ process/process.h
@@ -7,6 +7,14 @@
 /* state takes the following values
  *	0: not started yet
  */
+
+
+#define PIPE_STARTING_ADDRESS		0x20003f00
+#define PIPE_SIZE					32
+#define PIPE_NUM					5
+#define PIPE_SPACE					PIPE_NUM*PIPE_SIZE
+
+
 struct PCB{
 	//uint32_t stored_registers[PCB_REGISTERS_STORED];
 	uint32_t r0;
@@ -34,5 +42,10 @@ struct PCB{
 
 void createNewProcess(void *address);
 
+void initPipeSpace();
+static int used_pipes[PIPE_NUM];
+int createPipe();
+int writePipeByte(int pipe_id, uint8_t msg);
+int readPipeByte(int pipe_id);
 
 #endif
